b60a0f856a1f887f0e728332fc5dfe4b334081ac
Andebrhan <anteforedu@gmail.com>
2023-02-23 18:34:06 -0500

    spring boot framework added

---
+++ Spring-06-SpringBoot/src/main/java/com/cydeo/spring06springboot/Spring06SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring06springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring06SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring06SpringBootApplication.class, args);
+    }
+
+}
---
+++ Spring-06-SpringBoot/src/test/java/com/cydeo/spring06springboot/Spring06SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring06springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring06SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
