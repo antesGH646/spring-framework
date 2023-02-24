0897f1e5d98ed1bd9327609d7291d5080ffede5e
Andebrhan <anteforedu@gmail.com>
2023-02-24 15:04:38 -0500

    changed end point to user

--- Spring-07-MVC-Controler/src/main/java/com/cydeo/controller/UserController.java
+++ Spring-07-MVC-Controler/src/main/java/com/cydeo/controller/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class UserController {
 
-    @RequestMapping("/user/userinfo")
+    @RequestMapping("/user")
     public String user(){
         return "/user/userinfo.html";  //directs user folder under static folder
     }
