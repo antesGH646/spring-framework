2050182964787235da5030cf46f77385af33cb3f
Andebrhan <anteforedu@gmail.com>
2023-03-09 01:13:53 -0500

    exception classes added

---
+++ bank-simulation-app/src/main/java/com/cydeo/exception/BalanceNotSufficientException.java
@@ -0,0 +1,8 @@
+package com.cydeo.exception;
+
+public class BalanceNotSufficientException extends RuntimeException{
+
+    public BalanceNotSufficientException(String s) {
+        super(s);
+    }
+}
