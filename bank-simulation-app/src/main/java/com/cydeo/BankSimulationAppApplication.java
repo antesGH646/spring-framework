a6274f78073d1cd62d279ca390259d3a61a75138
Andebrhan <anteforedu@gmail.com>
2023-03-16 18:01:09 -0400

    business logic validation added and commented

--- bank-simulation-app/src/main/java/com/cydeo/BankSimulationAppApplication.java
+++ bank-simulation-app/src/main/java/com/cydeo/BankSimulationAppApplication.java
@@ -22,33 +22,38 @@ public class BankSimulationAppApplication {
         AccountService accountService = container.getBean(AccountService.class);
         TransactionService transactionService = container.getBean(TransactionService.class);
 
-        //create 2 account sender and receiver.
+        //create 2 account sender and receiver, different user id checking to checking
         Account sender = accountService.createNewAccount(BigDecimal.valueOf(70),new Date(), AccountType.CHECKING,1L);
-
         Account receiver = accountService.createNewAccount(BigDecimal.valueOf(50),new Date(), AccountType.CHECKING,2L);
-        Account receiver2 = null;
-        accountService.listAllAccount().forEach(System.out::println);
-
         transactionService.makeTransfer(sender,receiver,new BigDecimal(40),new Date(),"Transaction 1");
 
-        System.out.println(transactionService.findAllTransaction().get(0));
+//        //create 2 account sender and receiver, same user id checking to saving
+//        Account sender = accountService.createNewAccount(BigDecimal.valueOf(70),new Date(), AccountType.CHECKING,1L);
+//        Account receiver = accountService.createNewAccount(BigDecimal.valueOf(50),new Date(), AccountType.SAVING,1L);
+//        transactionService.makeTransfer(sender,receiver,new BigDecimal(40),new Date(),"Transaction 1");
+
+//        //create 2 account sender and receiver, different user id checking to saving, should throw exception
+//        Account sender = accountService.createNewAccount(BigDecimal.valueOf(70),new Date(), AccountType.CHECKING,1L);
+//        Account receiver = accountService.createNewAccount(BigDecimal.valueOf(50),new Date(), AccountType.SAVING,2L);
+//        transactionService.makeTransfer(sender,receiver,new BigDecimal(40),new Date(),"Transaction 1");
+
+//        //create 2 account sender and receiver, transferring higher amount should throw exception
+//        Account sender = accountService.createNewAccount(BigDecimal.valueOf(70),new Date(), AccountType.CHECKING,1L);
+//        Account receiver = accountService.createNewAccount(BigDecimal.valueOf(50),new Date(), AccountType.CHECKING,2L);
+//        transactionService.makeTransfer(sender,receiver,new BigDecimal(400),new Date(),"Transaction 1");
+
+        /*
+        Account receiver2 = null;//account assigned to null
+        //since receiver2 is null should throw exception
+        transactionService.makeTransfer(sender,receiver2,new BigDecimal(40),new Date(),"Transaction 1");
+        */
+
+        /*
+        in the properties file if the under_construction=true it should stop the execution
+         */
 
         accountService.listAllAccount().forEach(System.out::println);
-//        //get account and transaction service beans
-//        AccountService accountService = container.getBean(AccountService.class);
-//        TransactionService transactionService = container.getBean(TransactionService.class);
-//
-//        //create 2 account sender and receiver.
-//        Account sender = accountService.createNewAccount(BigDecimal.valueOf(70),new Date(), AccountType.CHECKING,1L);
-//
-//        Account receiver = accountService.createNewAccount(BigDecimal.valueOf(50),new Date(), AccountType.CHECKING,2L);
-//        Account receiver2 = null;
-//        accountService.listAllAccount().forEach(System.out::println);
-//
-//        transactionService.makeTransfer(sender,receiver,new BigDecimal(40),new Date(),"Transaction 1");
-//
-//        System.out.println(transactionService.findAllTransaction().get(0));
-//
-//        accountService.listAllAccount().forEach(System.out::println);
+        System.out.println(transactionService.findAllTransaction().get(0));
+        accountService.listAllAccount().forEach(System.out::println);
     }
 }
