7e93027a89e1b8e864729b085df62e74ae8f9d61
Andebrhan <anteforedu@gmail.com>
2023-03-15 21:25:47 -0400

    edited

--- bank-simulation-app/src/main/java/com/cydeo/service/AccountService.java
+++ bank-simulation-app/src/main/java/com/cydeo/service/AccountService.java
@@ -12,7 +12,7 @@ import java.util.List;
  * The common practice is to make services with abstract methods.
  * This class contains two abstract methods, the first abstract method is to create
  * an account with its required parameters. To create an account the account type,
- * the balance of the account, the date of creation, and a user id is required.
+ * the balance of the account, the date of creation, and a user id are required.
  * The second abstract method stores a list of the created accounts.
  */
 public interface AccountService {
