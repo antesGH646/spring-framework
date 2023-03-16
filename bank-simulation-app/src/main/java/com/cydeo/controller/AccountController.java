247a20f78a3f123f1b42e26b4a3cc5584efb8afe
Andebrhan <anteforedu@gmail.com>
2023-03-15 21:50:03 -0400

    new end point added

--- bank-simulation-app/src/main/java/com/cydeo/controller/AccountController.java
+++ bank-simulation-app/src/main/java/com/cydeo/controller/AccountController.java
@@ -21,4 +21,16 @@ public class AccountController {
         return "accounts/index";//returns the index html file from the accounts sub folder under the templates
     }
 
+    @GetMapping("/create-form")
+    public String getCreateForm(){
+
+        //empty account object provided
+        //account type enum needs to fill dropdown
+        return "account/create-account";
+    }
+
+    //create method to capture information from UI,
+    //print them on the console.
+    //trigger createAccount method, create the account based on user input.
+
 }
