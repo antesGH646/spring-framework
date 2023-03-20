b13ed08c5e990064546a5266df17e40ebedb82cf
Andebrhan <anteforedu@gmail.com>
2023-03-20 19:17:36 -0400

    edited

--- Spring-00-Core-Coupling/src/main/java/com/cydeo/tightly_coupled/CydeoApp.java
+++ Spring-00-Core-Coupling/src/main/java/com/cydeo/tightly_coupled/CydeoApp.java
@@ -2,12 +2,14 @@ package com.cydeo.tightly_coupled;
 
 public class CydeoApp {
     public static void main(String[] args) {
-        //making class instances to pass them as arguments, b/c mentor accepts two instances
+        //creating instances to pass them as arguments, b/c mentor accepts two instances
         FullTimeMentor fullTime = new FullTimeMentor();
         PartTimeMentor partTime = new PartTimeMentor();
 
         //Mentor the managing class accepts two class instances as an arguments
         Mentor mentor = new Mentor(fullTime,partTime);
-        mentor.manageAccount(); //calling the mentor method to run the component class methods
+
+        //calling the mentor method that runs the methods of the component classes
+        mentor.manageAccount();
     }
 }
