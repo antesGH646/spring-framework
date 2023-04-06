d582b20d5be4d20bc4178bc53a6f036e84a03ce7
Andebrhan <anteforedu@gmail.com>
2023-04-06 16:26:05 -0400

    modified added

--- Spring-10-ORM-TableCreation/src/main/resources/data.sql
+++ Spring-10-ORM-TableCreation/src/main/resources/data.sql
@@ -1,5 +1,10 @@
 INSERT INTO EMPLOYEES(id,name)
 VALUES(1,'Mike Smith');
 
---INSERT INTO student(first_name,last_name,email)
---VALUES('Mike',' Smith','mike@cydeo.com');
+-- INSERT INTO student(id, first_name,last_name,email)
+-- VALUES(1, 'Mike',' Smith','mike@cydeo.com');
+
+--postgres will provide the id(primary key) automatically, if you add the @Id, @Entity, and @GenerateValue()
+INSERT INTO student(first_name,last_name,email)
+VALUES('Mike',' Smith','mike@cydeo.com');
+
