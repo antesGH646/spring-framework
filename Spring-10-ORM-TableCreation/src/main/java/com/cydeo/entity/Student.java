cdb5357a2e842becd8972eb7faea4c72fbafe981
Andebrhan <anteforedu@gmail.com>
2023-04-06 16:22:10 -0400

    annotations added

--- Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Student.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Student.java
@@ -1,13 +1,36 @@
 package com.cydeo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import com.cydeo.Gender;
 
-@Entity
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity//to create a table from this class fields
+@Table(name = "Students")//to change the default table name
 public class Student {
-    @Id
+    @Id//when using @Entity, it is mandatory to use @Id (primary key)
+    //to provide primary keys automatically
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name="student_first_name")//to change default column names
     private String firstName;
+    @Column(name="student_last_name")//to change default column names
     private String lastName;
+
     private String email;
+
+    @Transient//do want to add this in the table columns
+    private String city;
+
+    //to add dates
+    @Column(columnDefinition = "Date")
+    private LocalDate birthDate;
+    @Column(columnDefinition = "Time")
+    private LocalDate birthTime;
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDate birthDateTime;
+
+    //using enums
+    @Enumerated(EnumType.STRING)//otherwise it will use integer, or ordinal is 0 & 1
+    private Gender gender;
 }
