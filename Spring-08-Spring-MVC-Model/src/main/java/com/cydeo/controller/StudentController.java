4c72698742a797a63cf9516c6cbcb7f164c76f35
Andebrhan <anteforedu@gmail.com>
2023-04-07 12:55:44 -0400

    modified

--- Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -15,17 +15,17 @@ public class StudentController {
     @RequestMapping("/welcome")
     public String student(Model model) {
         //want to past data to html, Model is an interface that carries data to view
-        model.addAttribute("name", "Cydeo");
+        model.addAttribute("name", "Cydeo");//name is an added model attribute carrying the String "Cydeo"
         model.addAttribute("course", "Back end Development");
 
         //create a random id 0-1000 and show it in the UI
         int studentId = new Random().nextInt();
-        model.addAttribute("id", studentId);
-        model.addAttribute("subject", "MVC");
+        model.addAttribute("id", studentId);//model attribute carrying the id
+        model.addAttribute("subject", "MVC");//model attribute carrying String "MVC"
 
         //display array lists or an element from a list in the UI
         List<Integer> numbers = Arrays.asList(12,41,52,51,65,32,26);
-        model.addAttribute("numbers", numbers);
+        model.addAttribute("numbers", numbers);//numbers is an added model attribute carrying the list
        // model.addAttribute("numbers", numbers.get(3));
 
         Student student = new Student(1, "Adam", "Smith");
@@ -34,10 +34,4 @@ public class StudentController {
         return "student/welcome"; //Thymeleaf template not an html file
     }
 
-    @RequestMapping("/welcome")
-    public String student2(Model model) {
-        Student student = new Student(1, "Adam", "Smith");
-        model.addAttribute("student", student);
-        return "student/welcome";
-    }
 }
