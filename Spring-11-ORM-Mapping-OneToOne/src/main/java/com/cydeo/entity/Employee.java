fe0ded65840b4649b57b26852a7946451a57de29
Andebrhan <anteforedu@gmail.com>
2023-04-13 18:40:48 -0400

    modified

--- Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/entity/Employee.java
+++ Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/entity/Employee.java
@@ -1,5 +1,6 @@
 package com.cydeo.entity;
 
+import com.cydeo.enums.Gender;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -15,29 +16,29 @@ public class Employee extends BaseEntity{
     private String firstName;
     private String lastName;
     private String email;
-    @Column(columnDefinition = "Date")
-    private LocalDate hire_date;
-    private String gender;
+    @Column(columnDefinition = "DATE")
+    private LocalDate hireDate;
     private int salary;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
 
     @OneToOne(cascade = CascadeType.ALL)
-   // @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+//    @OneToOne(cascade = {CascadeType.PERSIST,CascadeType.REMOVE})
     @JoinColumn(name = "department_id")
     private Department department;
 
     @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name="region_id")
+    @JoinColumn(name = "region_id")
     private Region region;
 
-    public Employee(String firstName, String lastName,
-                    String email, LocalDate hire_date,
-                    String gender, int salary) {
+    public Employee(String firstName, String lastName, String email,
+                    LocalDate hireDate, int salary, Gender gender) {
 
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
-        this.hire_date = hire_date;
-        this.gender = gender;
+        this.hireDate = hireDate;
         this.salary = salary;
+        this.gender = gender;
     }
 }
--- Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/entity/Region.java
+++ Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/entity/Region.java
@@ -1,20 +1,23 @@
 package com.cydeo.entity;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name="regions")
+@NoArgsConstructor
 @Data
-public class Region {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+public class Region extends BaseEntity{
     private String region;
     private String country;
 
+    @OneToOne(mappedBy = "region")
+    private Employee employee;
 
+    public Region(String region, String country) {
+        this.region = region;
+        this.country = country;
+    }
 }
