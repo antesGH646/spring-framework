89c7d5bf342278555dc55b47b0e77718c2b8e4ab
Andebrhan <anteforedu@gmail.com>
2023-04-13 15:12:15 -0400

    changed to LocalDate &  custom column name

--- Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/entity/Employee.java
+++ Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/entity/Employee.java
@@ -2,11 +2,8 @@ package com.cydeo.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import java.util.Date;
+import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @Data
@@ -18,7 +15,8 @@ public class Employee {
     private String firstName;
     private String lastName;
     private String email;
-    private Date hire_date;
+    @Column(columnDefinition = "Date")
+    private LocalDate hire_date;
     private String department;
     private String gender;
     private int salary;
