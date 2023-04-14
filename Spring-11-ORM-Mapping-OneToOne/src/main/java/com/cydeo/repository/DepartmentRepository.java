9afe0933259a06b51f5d7243c0d35570d9cf42df
Andebrhan <anteforedu@gmail.com>
2023-04-13 20:48:57 -0400

    deleted due to cascading

--- Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/repository/DepartmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cydeo.repository;
-
-import com.cydeo.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//pass the entity name and the primary type
-public interface DepartmentRepository extends JpaRepository<Department, Integer> {
-}
