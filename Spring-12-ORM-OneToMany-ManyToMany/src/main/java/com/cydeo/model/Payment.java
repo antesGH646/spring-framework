4c3b1498ccc6acc20dad88f234087d1da132a422
Andebrhan <anteforedu@gmail.com>
2023-04-16 00:41:45 -0400

    one-to-many relationship created

---
+++ Spring-12-ORM-OneToMany-ManyToMany/src/main/java/com/cydeo/model/Payment.java
@@ -0,0 +1,38 @@
+package com.cydeo.model;
+
+import com.cydeo.enums.Status;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "payments")
+@Data
+@NoArgsConstructor
+public class Payment extends BaseEntity {
+
+    @Column(columnDefinition = "Date")
+    private LocalDate date;
+    private BigDecimal amount;
+    private Status status;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private PaymentDetail paymentDetail;
+
+    @ManyToOne//no good to put cascading in Many-to-one relationship
+    private Merchant merchant;
+
+    @ManyToOne
+    private Customer customer;
+
+    //Constructor, id and relationship building objects are not included
+
+    public Payment(LocalDate date, BigDecimal amount, Status status) {
+        this.date = date;
+        this.amount = amount;
+        this.status = status;
+    }
+}
