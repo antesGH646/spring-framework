fca2578240198501649f0524a5f2e56efda4f497
Andebrhan <anteforedu@gmail.com>
2023-04-20 20:34:56 -0400

    query orm entities enums sql and repository created

--- Spring-12-ORM-OneToMany-ManyToMany/src/main/java/com/cydeo/repository/PaymentRepository.java
+++ Spring-12-ORM-OneToMany-ManyToMany/src/main/java/com/cydeo/repository/PaymentRepository.java
@@ -3,5 +3,11 @@ package com.cydeo.repository;
 import com.cydeo.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * There are three ways to retrieve data from database tabes
+ *  1) drive query: creating a method: syntax => findByfieldName(parameter) queries
+ *  2) writing sq queries: uses @Query annotation
+ *  3)PSQL(persistent sql query language): uses @Query annotation
+ */
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 }
