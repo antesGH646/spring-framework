8d70439dc6f7279d2d88321efed34bc5cf0400e0
Andebrhan <anteforedu@gmail.com>
2023-05-04 16:32:17 -0400

    repositories data retrieving

--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/CinemaRepository.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/CinemaRepository.java
@@ -2,6 +2,8 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Cinema;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -25,15 +27,26 @@ public interface CinemaRepository extends JpaRepository<Cinema, Long> {
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query to read the cinema name with a specific id
+    @Query("select c from Cinema c where c.id = ?1")
+    String findByName(@Param("id") Long id);//when id is passed it returns a string of a Cinema name
 
     // ------------------- Native QUERIES ------------------- //
 
-    //Write a native query to read all cinemas by location country
+    //Write a native query to read all cinemas by location country (return a list of cinemas by a country)
+    @Query(value = "SELECT * FROM cinema c JOIN location l ON c.location_id = l.id WHERE l.country = ?1",nativeQuery = true)
+    List<Cinema> findAllCinemasBasedOnLocationCountry(@Param("locationCountry") String locationCountry);
 
     //Write a native query to read all cinemas by name or sponsored name contains a specific pattern
+    @Query(value = "SELECT * FROM cinema WHERE name ILIKE concat('%',?1,'%') OR " +
+            "sponsored_name ILIKE concat('%',?1,'%')",nativeQuery = true)
+    List<Cinema> findAllCinemasByNameOrSponsoredName(@Param("pattern") String pattern);
 
     //Write a native query to sort all cinemas by name
+    @Query(value = "SELECT * FROM cinema ORDER BY name ",nativeQuery = true)
+    List<Cinema> sortByName();
 
     //Write a native query to distinct all cinemas by sponsored name
+    @Query(value = "SELECT DISTINCT sponsored_name FROM cinema",nativeQuery = true)
+    List<String> findDistinctCinemasBySponsoredName();
 
 }
--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/UserRepository.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/UserRepository.java
@@ -2,35 +2,48 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     // ------------------- DERIVED QUERIES ------------------- //
 
     //Write a derived query to read a user with an email?
+    Optional<User> findByEmail(String email);
 
-    //Write a derived query to read a user with an username?
-
+    //Write a derived query to read a user with a username?
+    Optional<User> findByUsername(String email);
     //Write a derived query to list all users that contain a specific name?
-
+     List<User> findUsersByAccountContains(String name);
     //Write a derived query to list all users that contain a specific name in the ignore case mode?
-
+    List<User> findUsersByAccountContainingIgnoreCase(String name);
     //Write a derived query to list all users with an age greater than a specified age?
 
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query that returns a user read by email?
-
+    @Query("select u from User u where u.email = ?1 ")
+    Optional<User> fetchUserByEmail(@Param("user") String email);
     //Write a JPQL query that returns a user read by username?
-
+    @Query("select u from User u where u.username = ?1 ")
+    Optional<User> fetchUserByUser(@Param("username") String email);
     //Write a JPQL query that returns all users?
 
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query that returns all users that contain a specific name?
-
+    @Query(value = "select * from users ilike concat('%',?1,'%') ",nativeQuery = true)
+    List<User> getUserByAccountNameContaining(String name);
     //Write a native query that returns all users?
-
+    @Query(value = "select * from user", nativeQuery = true)
+    List<User> findAllUsers();
     //Write a native query that returns all users in the range of ages?
-
+    @Query(value = "select ad from AccountDetails where ad.age between ?1 and ?2", nativeQuery = true)
+    List<User> findUsersByAccountDetailsAgeRange(@Param("age1") Integer age1, @Param("age2") Integer age2);
     //Write a native query to read a user by email?
+    @Query(value = "select u from User u where u.email = ?1",nativeQuery = true)
+    Optional<User> getUserByEmail(String email);
 }
