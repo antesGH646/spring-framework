9b1ae2b9f107b95d075e4ab2f0502768dcc2ae39
Andebrhan <anteforedu@gmail.com>
2023-05-15 11:56:38 -0400

    modified

--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -2,7 +2,11 @@ package com.cydeo.bootstrap;
 
 import com.cydeo.repository.*;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
+@Component
 public class DataGenerator implements CommandLineRunner {
 
     private final AccountRepository accountRepository;
@@ -13,7 +17,7 @@ public class DataGenerator implements CommandLineRunner {
     private final TicketRepository ticketRepository;
     private final UserRepository userRepository;
 
-    public DataGenerator(AccountRepository accountRepository, CinemaRepository cinemaRepository, GenreRepository genreRepository, MovieCinemaRepository movieCinemaRepository, MovieRepository movieRepository, TicketRepository ticketRepository, UserRepository userRepository) {
+    public DataGenerator(AccountRepository accountRepository, CinemaRepository cinemaRepository, MovieRepository movieRepository, MovieCinemaRepository movieCinemaRepository, TicketRepository ticketRepository, GenreRepository genreRepository, UserRepository userRepository) {
         this.accountRepository = accountRepository;
         this.cinemaRepository = cinemaRepository;
         this.genreRepository = genreRepository;
@@ -26,5 +30,18 @@ public class DataGenerator implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+        System.out.println(cinemaRepository.findByName("Hall 1 - EMPIRE"));
+        System.out.println(accountRepository.getAllAdminAccounts());
+        System.out.println(cinemaRepository.findDistinctCinemasBySponsoredName());
+        System.out.println(movieRepository.fetchAllMovieByName());
+        System.out.println(movieCinemaRepository.countAllByCinemaId(4L));
+        System.out.println(movieCinemaRepository.findAllByCinema_Location_Name("AMC Empire 25"));
+        System.out.println(ticketRepository.fetchAllTicketsByUserAccount(4L));
+        System.out.println(ticketRepository.fetchAllTicketsBetweenRangeOfDateTimes(LocalDateTime.now().minusDays(1000), LocalDateTime.now()));
+        System.out.println(ticketRepository.countAllByMovieCinema_Movie_Name("it"));
+        System.out.println(genreRepository.findAllGenres());
+        System.out.println(userRepository.fetchAllUsers());
+
     }
 }
+
--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/UserRepository.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/repository/UserRepository.java
@@ -17,11 +17,12 @@ public interface UserRepository extends JpaRepository<User, Long> {
     //Write a derived query to read a user with a username?
     Optional<User> findByUsername(String email);
     //Write a derived query to list all users that contain a specific name?
-     List<User> findUsersByAccountContains(String name);
+     List<User> findAllByAccountNameContaining(String name);
     //Write a derived query to list all users that contain a specific name in the ignore case mode?
-    List<User> findUsersByAccountContainingIgnoreCase(String name);
-    //Write a derived query to list all users with an age greater than a specified age?
+    List<User> findAllByAccountNameContainingIgnoreCase(String name);
 
+    //Write a derived query to list all users with an age greater than a specified age?
+    List<User> findAllByAccount_AgeGreaterThan(Integer age);
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query that returns a user read by email?
@@ -31,19 +32,27 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query("select u from User u where u.username = ?1 ")
     Optional<User> fetchUserByUser(@Param("username") String email);
     //Write a JPQL query that returns all users?
+    @Query("SELECT u FROM User u")
+    List<User> fetchAllUsers();
 
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query that returns all users that contain a specific name?
-    @Query(value = "select * from users ilike concat('%',?1,'%') ",nativeQuery = true)
-    List<User> getUserByAccountNameContaining(String name);
+    @Query(value = "SELECT * FROM user_account u JOIN account_details ad " +
+            "ON u.account_details_id = ad.id WHERE ad.name ILIKE concat('%',?1,'%') ",nativeQuery = true)
+    List<User> retrieveAllByName(@Param("name") String name);
+
     //Write a native query that returns all users?
-    @Query(value = "select * from user", nativeQuery = true)
-    List<User> findAllUsers();
+    @Query(value = "SELECT * FROM user_account",nativeQuery = true)
+    List<User> retrieveAll();
+
     //Write a native query that returns all users in the range of ages?
-    @Query(value = "select ad from AccountDetails where ad.age between ?1 and ?2", nativeQuery = true)
-    List<User> findUsersByAccountDetailsAgeRange(@Param("age1") Integer age1, @Param("age2") Integer age2);
+    @Query(value = "SELECT * FROM user_account u JOIN account_details ad " +
+            " ON ad.id = u.account_details_id " +
+            "WHERE ad.age BETWEEN ?1 AND ?2",nativeQuery = true)
+    List<User> retrieveBetweenAgeRange(@Param("age1") Integer age1, @Param("age2") Integer age2);
+
     //Write a native query to read a user by email?
-    @Query(value = "select u from User u where u.email = ?1",nativeQuery = true)
-    Optional<User> getUserByEmail(String email);
+    @Query(value = "SELECT * FROM user_account WHERE email = ?1",nativeQuery = true)
+    User retrieveByEmail(@Param("email") String email);
 }
