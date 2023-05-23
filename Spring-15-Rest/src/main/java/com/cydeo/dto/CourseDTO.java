9923c715ac1dcdebb455185833336b7dbf5f0f58
Andebrhan <anteforedu@gmail.com>
2023-05-23 14:19:38 -0400

    spaces removed

--- Spring-15-Rest/src/main/java/com/cydeo/dto/CourseDTO.java
+++ Spring-15-Rest/src/main/java/com/cydeo/dto/CourseDTO.java
@@ -9,19 +9,10 @@ import lombok.Setter;
 @Setter
 public class CourseDTO {
 
-
     private Long id;
-
-
     private String name;
-
-
     private String category;
-
-
     private int rating;
-
-
     private String description;
 
 }
