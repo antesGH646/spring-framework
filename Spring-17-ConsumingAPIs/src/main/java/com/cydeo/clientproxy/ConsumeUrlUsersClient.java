cb6d267ac195998ed19664a5a0a2c90e47879549
Andebrhan <anteforedu@gmail.com>
2023-05-25 14:35:16 -0400

    consume unsecure url

---
+++ Spring-17-ConsumingAPIs/src/main/java/com/cydeo/clientproxy/ConsumeUrlUsersClient.java
@@ -0,0 +1,14 @@
+package com.cydeo.clientproxy;
+
+import com.cydeo.dto.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@FeignClient(url = "https://jsonplaceholder.typicode.com", name = "USERS-CLIENT")//base url of the consumed url
+public interface ConsumeUrlUsersClient {
+    //when you call this method will hit the above URL and trigger the Get method
+    @GetMapping("/users")
+    List<User> getUsers();
+}
--- Spring-17-ConsumingAPIs/src/main/java/com/cydeo/controller/Consume_FeignClient.java
+++ Spring-17-ConsumingAPIs/src/main/java/com/cydeo/controller/Consume_FeignClient.java
@@ -1,5 +1,30 @@
 package com.cydeo.controller;
 
-public class Consume_FeignClient {
+import com.cydeo.clientproxy.ConsumeSecureUrlClient;
+import com.cydeo.clientproxy.ConsumeUrlUsersClient;
+import com.cydeo.dto.ResponseWrapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class Consume_FeignClient {
+    private final ConsumeUrlUsersClient userClient;
+    private final ConsumeSecureUrlClient employeeClient;
+
+    public Consume_FeignClient(ConsumeUrlUsersClient userClient,
+                               ConsumeSecureUrlClient employeeClient) {
+        this.userClient = userClient;
+        this.employeeClient = employeeClient;
+    }
+
+    @GetMapping("/api/v1/users")
+    public ResponseEntity<ResponseWrapper> getUsers(){
+        return ResponseEntity.ok(new ResponseWrapper("Retrieved list of users", userClient.getUsers()));
+    }
+
+    @GetMapping("/api/v1/employee")
+    public ResponseEntity<ResponseWrapper> getEmployee() {
+        return ResponseEntity.ok(new ResponseWrapper("Retrieved Employee ", employeeClient.getEmployee("6298ebfecd0551211fce37a6")));
+    }
 }
