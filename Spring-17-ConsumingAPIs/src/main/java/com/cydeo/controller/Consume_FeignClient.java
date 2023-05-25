0185b90ba0e0aa5a30ffb0bd314ae55f7c940b77
Andebrhan <anteforedu@gmail.com>
2023-05-25 16:32:33 -0400

    FeignClient completed

--- Spring-17-ConsumingAPIs/src/main/java/com/cydeo/controller/Consume_FeignClient.java
+++ Spring-17-ConsumingAPIs/src/main/java/com/cydeo/controller/Consume_FeignClient.java
@@ -18,12 +18,12 @@ public class Consume_FeignClient {
         this.employeeClient = employeeClient;
     }
 
-    @GetMapping("/api/v1/users")
+    @GetMapping("/api/v1/users")//http://localhost:8080/api/v1/users
     public ResponseEntity<ResponseWrapper> getUsers(){
         return ResponseEntity.ok(new ResponseWrapper("Retrieved list of users", userClient.getUsers()));
     }
 
-    @GetMapping("/api/v1/employee")
+    @GetMapping("/api/v1/employee")//http://localhost:8080/api/v1/employee
     public ResponseEntity<ResponseWrapper> getEmployee() {
         return ResponseEntity.ok(new ResponseWrapper("Retrieved Employee ", employeeClient.getEmployee("6298ebfecd0551211fce37a6")));
     }
