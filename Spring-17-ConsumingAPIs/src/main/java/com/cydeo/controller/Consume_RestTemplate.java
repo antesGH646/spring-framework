2305aa968a88885cc8035c1d0d7da685750f0546
Andebrhan <anteforedu@gmail.com>
2023-05-25 13:30:47 -0400

    RestTemplate completed

--- Spring-17-ConsumingAPIs/src/main/java/com/cydeo/controller/Consume_RestTemplate.java
+++ Spring-17-ConsumingAPIs/src/main/java/com/cydeo/controller/Consume_RestTemplate.java
@@ -1,12 +1,14 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.User;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/users")
@@ -29,7 +31,7 @@ public class Consume_RestTemplate {
      * the Json body and maps it to this dto and accepts the URI and
      * the Array class that you want to map into.
      */
-    @GetMapping
+    @GetMapping//http://localhost:8080/users
     public User[] readAllUsers() {
         ResponseEntity<User[]> responseEntity = restTemplate.getForEntity(URI, User[].class);
         return responseEntity.getBody();
@@ -44,9 +46,36 @@ public class Consume_RestTemplate {
      *  @param id Integer, pass integer endpoint
      *  @return Object
      */
-    @GetMapping("{id}")
+    @GetMapping("{id}")//http://localhost:8080/users/2
     public Object fetchUser(@PathVariable("id") Integer id) {
         String URL = URI + "/{id}";
         return restTemplate.getForObject(URL, Object.class,id);
+    }
+
+    /**
+     * When consuming a third party API, some API require to enter some
+     * headers for security purposes. HttpHeaders class works with headers.
+     * exchange() method from RestTemplate is used when you want to pass
+     * something.
+     * It accepts:
+     *     -the URL that you want to consume
+     *     -type of Http method
+     *     -Http entity object
+     *     -class name
+     * @return ResponseEntity Object
+     */
+    @GetMapping("/test")
+    public ResponseEntity<Object> consumeFromDummyApi() {
+        //creating Http header to pass the key and values
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.set("app-id","6298ebfecd0551211fce37a6");
+        //creating the HttpEntity to pass the Header
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        //Creating ResponseEntity to pass the HttPEntity
+        ResponseEntity<Object> response =
+                restTemplate.exchange("https://dummyapi.io/data/v1/user/?limit=10", HttpMethod.GET,entity, Object.class);
+       //returning the response entity
+        return response;
     }
 }
