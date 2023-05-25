2beb41a7191d0e5b033af0068bcf6cbd7da5229b
Andebrhan <anteforedu@gmail.com>
2023-05-24 20:44:57 -0400

    creating RestTemplate bean

--- Spring-17-ConsumingAPIs/src/main/java/com/cydeo/Spring17ConsumingApIsApplication.java
+++ Spring-17-ConsumingAPIs/src/main/java/com/cydeo/Spring17ConsumingApIsApplication.java
@@ -2,6 +2,8 @@ package com.cydeo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Spring17ConsumingApIsApplication {
@@ -10,4 +12,8 @@ public class Spring17ConsumingApIsApplication {
         SpringApplication.run(Spring17ConsumingApIsApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
