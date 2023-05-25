9083d7e3076617d2562379837d33d737bb73a762
Andebrhan <anteforedu@gmail.com>
2023-05-25 19:25:45 -0400

    2nd WebClient API endpoint created

--- Spring-17-ConsumingAPIs/src/main/java/com/cydeo/controller/Consume_WebClient.java
+++ Spring-17-ConsumingAPIs/src/main/java/com/cydeo/controller/Consume_WebClient.java
@@ -4,8 +4,10 @@ import com.cydeo.entity.MovieCinema;
 import com.cydeo.repository.GenreRepository;
 import com.cydeo.repository.MovieCinemaRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class Consume_WebClient {
@@ -19,8 +21,13 @@ public class Consume_WebClient {
         this.genreRepository = genreRepository;
     }
 
-    @GetMapping("/flux-movie-cinema")
+    @GetMapping("/flux-movie-cinema")//http://localhost:8080/flux-movie-cinema
     public Flux<MovieCinema> readAllMovieCinemaFlux() {
         return Flux.fromIterable(movieCinemaRepository.findAll());
+    }
+
+    @GetMapping("/mono-movie-cinema/{id}")//http://localhost:8080/mono-movie-cinema/3
+    public Mono<MovieCinema> readMovieCinemaById(@PathVariable("id") Long id) {
+        return Mono.just(movieCinemaRepository.findById(id).get());//.get() added b/c it is Optional
     }
 }
