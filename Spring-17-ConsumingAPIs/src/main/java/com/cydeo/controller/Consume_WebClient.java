af4a72eccea38f3de9e167ec757df5d28127e0e3
Andebrhan <anteforedu@gmail.com>
2023-05-25 20:15:36 -0400

    3rd WebClient API endpoint created

--- Spring-17-ConsumingAPIs/src/main/java/com/cydeo/controller/Consume_WebClient.java
+++ Spring-17-ConsumingAPIs/src/main/java/com/cydeo/controller/Consume_WebClient.java
@@ -1,11 +1,11 @@
 package com.cydeo.controller;
 
+import com.cydeo.entity.Genre;
 import com.cydeo.entity.MovieCinema;
 import com.cydeo.repository.GenreRepository;
 import com.cydeo.repository.MovieCinemaRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -26,8 +26,26 @@ public class Consume_WebClient {
         return Flux.fromIterable(movieCinemaRepository.findAll());
     }
 
+//    @GetMapping("/mono-movie-cinema/{id}")//http://localhost:8080/mono-movie-cinema/3
+//    public Mono<MovieCinema> readMovieCinemaById(@PathVariable("id") Long id) {
+//        return Mono.just(movieCinemaRepository.findById(id).get());//.get() added b/c it is Optional
+//    }
+
+    //can create and endpoint using ResponseEntity too
     @GetMapping("/mono-movie-cinema/{id}")//http://localhost:8080/mono-movie-cinema/3
-    public Mono<MovieCinema> readMovieCinemaById(@PathVariable("id") Long id) {
-        return Mono.just(movieCinemaRepository.findById(id).get());//.get() added b/c it is Optional
+    public ResponseEntity<Mono<MovieCinema>> getMovieCinemaById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(Mono.just(movieCinemaRepository.findById(id).get()));//.get() added b/c it is Optional
+    }
+
+    @PostMapping("/create-genre")
+    public Mono<Genre> createGenre(@RequestBody Genre genre) {
+        Genre createGenre = genreRepository.save(genre);
+        return Mono.just(createGenre);
+    }
+
+    @DeleteMapping("/delete/genre/{id}")
+    public Mono<Void> deleteGenre(@PathVariable("id") Long id){
+        genreRepository.deleteById(id);
+        return Mono.empty();
     }
 }
