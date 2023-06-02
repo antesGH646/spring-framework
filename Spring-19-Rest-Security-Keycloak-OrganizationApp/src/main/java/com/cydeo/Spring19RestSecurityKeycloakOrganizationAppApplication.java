a370b1897902d0ed72bad948764ed4f4f5beee51
Andebrhan <anteforedu@gmail.com>
2023-06-02 17:47:09 -0400

    security keycloak organization app started

---
+++ Spring-19-Rest-Security-Keycloak-OrganizationApp/src/main/java/com/cydeo/Spring19RestSecurityKeycloakOrganizationAppApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring19RestSecurityKeycloakOrganizationAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring19RestSecurityKeycloakOrganizationAppApplication.class, args);
+    }
+
+}
