caf25ab8036f6acef276c8c102161b6b7e0ec67e
Andebrhan <anteforedu@gmail.com>
2023-06-23 00:36:15 -0400

    create empty tests and run

--- Spring-21-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
+++ Spring-21-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
@@ -5,6 +5,31 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest {
 
     @Test
+    void testCase1() {
+
+    }
+
+    @Test
+    void testCase2() {
+
+    }
+
+    @Test
+    void testCase3() {
+
+    }
+
+    @Test
+    void testCase4() {
+
+    }
+
+    @Test
+    void testCase5() {
+
+    }
+
+    @Test
     void add() {
         System.out.println("add");
         int actual = Calculator.add(2, 3);
