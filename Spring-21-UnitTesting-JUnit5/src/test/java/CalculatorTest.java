ec8cce181778e0f92a1d48127897e80fc22690d6
Andebrhan <anteforedu@gmail.com>
2023-06-23 13:41:14 -0400

    before/after each/all added

--- Spring-21-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
+++ Spring-21-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
@@ -4,14 +4,35 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+    @BeforeEach
+    void runningBefore() {
+        System.out.println("Lines of code defined in @BeforeEach will run before this test");
+    }
+
+    @AfterEach
+    void runningAfter() {
+        System.out.println("Lines of code defined in @AfterEach will run after this test\n");
+    }
+
+    @BeforeAll
+    static void runFirstSetUp() {
+        System.out.println("Lines of code defined in @BeforeAll will run before all tests\n");
+    }
+
+    @AfterAll
+    static void runLastTearDown() {
+        System.out.println("Lines of code defined in @AfterAll will run after all tests");
+    }
     @Test
     void testCase1() {
+        System.out.println("Test 1");
         assertTrue(Calculator.add(2,4) == 6);
         assertFalse(Calculator.add(2,4) == 7);
     }
 
     @Test
     void testCase2() {
+        System.out.println("Test 2");
         assertTrue(Calculator.operator.equals("add"));
         assertEquals("add", Calculator.operator);
 
@@ -19,6 +40,7 @@ class CalculatorTest {
 
     @Test
     void testCase3() {
+        System.out.println("Test 3");
        //if both array has the same size, order, and content they are equal
         assertArrayEquals(new int[]{1,2,3}, new int[]{1,2,3});
        // assertArrayEquals(new int[]{1,2,3}, new int[]{1,3,2});
@@ -26,6 +48,7 @@ class CalculatorTest {
 
     @Test
     void testCase4() {
+        System.out.println("Test 4");
       String n = null;
       String s = "my";
       assertNull(n);
@@ -44,9 +67,26 @@ class CalculatorTest {
 
     @Test
     void add() {
-        System.out.println("add");
+        System.out.println("add()");
         int actual = Calculator.add(2, 3);
         //compares expected vs actual otherwise throws the message
         assertEquals(5, actual, "It is not matching with expected value.");
+    }
+
+    /**
+     * Any method that implements the execute() method of Executable interface
+     * must throw exception, and add2() method is throwing an exception.
+     * Assertion.assertThrows() accepts the expected exception type
+     * and the method that is throwing the exception as a lambda function, why?
+     * because Executable is defined as a functional interface.
+     * NB. Consider the inheritance when calling methods
+     */
+    @Test
+    void add2() {
+        System.out.println("add2()");
+        //IllegalArgumentException is the expected exception, so the test should pass
+        assertThrows(IllegalArgumentException.class, () -> Calculator.add2(5,2));
+        //NullPointerException is not the expected exception, so the test should fail
+       // assertThrows(NullPointerException.class, () -> Calculator.add2(5,2));
     }
 }
