1dcf4e1cb45017c06d0137c194c1cbba0cfd16a0
Andebrhan <anteforedu@gmail.com>
2023-07-25 16:42:50 -0400

    comment added

--- Spring-00-Core-Coupling/src/main/java/com/cydeo/loosely_coupled/CydeoApp.java
+++ Spring-00-Core-Coupling/src/main/java/com/cydeo/loosely_coupled/CydeoApp.java
@@ -3,8 +3,11 @@ package com.cydeo.loosely_coupled;
 public class CydeoApp {
 
     public static void main(String[] args) {
+        //create objects of the implementing classes
         FullTimeMentor fullTime = new FullTimeMentor();
         //PartTimeMentor partTime = new PartTimeMentor();
+
+        //pass the object to pass the implementing method of the object
         MentorAccount mentor = new MentorAccount(fullTime);
         mentor.manageAccount();
     }
