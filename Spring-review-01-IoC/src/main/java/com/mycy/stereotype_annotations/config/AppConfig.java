e879ceddefe99f44052e6e72824712eae479c3c2
Andebrhan <anteforedu@gmail.com>
2023-08-31 15:23:02 -0400

    modified

--- Spring-review-01-IoC/src/main/java/com/mycy/stereotype_annotations/config/AppConfig.java
+++ Spring-review-01-IoC/src/main/java/com/mycy/stereotype_annotations/config/AppConfig.java
@@ -3,11 +3,11 @@ package com.mycy.stereotype_annotations.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Inside the stereotype annotation config file no need to add the object
+ */
 @Configuration//tells the Spring Container to follow the direction below
 //tells the Spring Container to look for classes inside stereotype_annotations package
-@ComponentScan(basePackages = "stereotype_annotations")
+@ComponentScan()
 public class AppConfig {
-    /*
-    Inside the stereotype annotation config file no need to add the object
-     */
 }
