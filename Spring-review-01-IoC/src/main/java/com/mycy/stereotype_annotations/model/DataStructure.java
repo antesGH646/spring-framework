c494b4ffd8e316e078b690bceaa92dc6f134a32c
Andebrhan <anteforedu@gmail.com>
2023-08-31 15:46:16 -0400

    modified

--- Spring-review-01-IoC/src/main/java/com/mycy/stereotype_annotations/model/DataStructure.java
+++ Spring-review-01-IoC/src/main/java/com/mycy/stereotype_annotations/model/DataStructure.java
@@ -4,7 +4,6 @@ import lombok.*;
 import org.springframework.stereotype.Component;
 
 /**
- **
  * With stereotype annotation, no need to create methods/objects
  *  limitation with stereotype annotation:
  *      - we cannot use multiple object as we use with bean annotations.
