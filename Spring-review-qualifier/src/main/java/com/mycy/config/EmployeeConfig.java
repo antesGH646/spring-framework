ac6e26a8e1b5fad3352411b75e1b38be3911972b
Andebrhan <anteforedu@gmail.com>
2023-11-08 18:44:13 -0500

    renamed

--- Spring-review-qualifier/src/main/java/com/mycy/config/EmployeeConfig.java
+++ Spring-review-qualifier/src/main/java/com/mycy/config/EmployeeConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.mycy")//will look all classes with @Component annotation inside the cydeo package
+@ComponentScan("com.mycy")//will look all classes with @Component annotation inside the mycy package
 public class EmployeeConfig {
 }
