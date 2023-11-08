94e6c098a2a3afe4e5caf8aa4f17c6aed2c055e3
Andebrhan <anteforedu@gmail.com>
2023-11-08 18:56:48 -0500

    comments edited

--- Spring-03-Core-DI/src/main/java/com/mycy/Java.java
+++ Spring-03-Core-DI/src/main/java/com/mycy/Java.java
@@ -11,12 +11,13 @@ public class Java {
   //  @Autowired //using autowired on field level, but it will be depreciated soon
     OfficeHours officeHours; //Java do not know about it unless you inject it
 
-    /**
-      if you are not using lombok annotation e.g the above @AllArgsConstructor
-      you need to create a constructor and use @Autowired on the constructor level,
-      otherwise Spring will throw a null pointer exception.
-      If you are using lombok annotation Spring will inject the bean automatically
-     */
+  /**
+   * if you are NOT using lombok annotation e.g the above @AllArgsConstructor
+   * you need to create a constructor and use @Autowired on the constructor level,
+   * otherwise Spring will throw a null pointer exception.
+   * However, if you are using lombok annotation Spring will inject the bean automatically
+   * no need to create a constructor and Add @Autowired annotation
+   */
 //     @Autowired
 //    public Java(OfficeHours officeHours) {
 //        this.officeHours = officeHours;
--- Spring-03-Core-DI/src/main/java/com/mycy/Selenium.java
+++ Spring-03-Core-DI/src/main/java/com/mycy/Selenium.java
@@ -7,7 +7,14 @@ import org.springframework.stereotype.Component;
 public class Selenium {
     OfficeHours officeHours;
 
-    @Autowired //to tell the Spring to create a bean of the OfficeHours
+    /**
+     * Since lombok annotation is not used in this component class, you need to create
+     * a constructor and add the @Autowired annotation on the constructor level,
+     * otherwise, Spring will throw a null pointer exception.
+     * However, if you are using lombok annotations on the class level, Spring will inject
+     * the bean automatically, you don't need to create a constructor and add @Autowired annotation.
+     */
+    @Autowired //tells Spring to create a bean and autowire it when called
     public Selenium(OfficeHours officeHours) {
         this.officeHours = officeHours;
     }
